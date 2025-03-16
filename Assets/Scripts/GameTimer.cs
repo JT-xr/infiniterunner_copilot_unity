c0f8a6a7b2f811eb45965f29848d0214c3af6ef1
JT <james4business@hotmail.com>
2025-03-15 20:08:33 -0400

    Create game timer script

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    public Text timerText;
+    private float timeRemaining = 60f;
+    private bool timerIsRunning = false;
+
+    private void Start()
+    {
+        // Start the timer
+        timerIsRunning = true;
+        UpdateTimerText();
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerText();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                UpdateTimerText();
+            }
+        }
+    }
+
+    private void UpdateTimerText()
+    {
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = "clock\n:" + seconds.ToString("00");
+
+        // Change color to red when time is less than or equal to 20 seconds
+        if (timeRemaining <= 20)
+        {
+            timerText.color = Color.red;
+        }
+    }
+}
