463265ad2e8da94fc80c605e36616b50770c8430
JT <james4business@hotmail.com>
2025-04-08 21:31:51 -0400

    Updated Player Controller script to move catcher item correctly; other updates in description
    Updated values for score and timer text;
    Updated Input Settings to use both legacy and new systems;

--- infiniterunner_copilot/Assets/Scripts/PlayerController.cs
+++ infiniterunner_copilot/Assets/Scripts/PlayerController.cs
@@ -2,39 +2,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 8f; // Moderate movement speed
+    [SerializeField] private float moveSpeed = 8f; // Movement speed
     private float screenBoundaryLeft;
     private float screenBoundaryRight;
     private float playerWidth;
+    private RectTransform rectTransform;
 
     void Start()
     {
-        // Calculate screen boundaries in world coordinates
-        Camera mainCamera = Camera.main;
-        playerWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
-        
-        Vector2 screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        screenBoundaryLeft = -screenBounds.x + playerWidth;
-        screenBoundaryRight = screenBounds.x - playerWidth;
+        // Get the RectTransform component
+        rectTransform = GetComponent<RectTransform>();
 
-        // Position player at the bottom of the screen
-        Vector3 startPosition = transform.position;
-        startPosition.y = -screenBounds.y + playerWidth;
-        transform.position = startPosition;
+        // Calculate the player's width
+        playerWidth = rectTransform.rect.width / 2;
+
+        // Calculate screen boundaries in canvas space
+        RectTransform canvasRect = rectTransform.GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        screenBoundaryLeft = -canvasRect.rect.width / 2 + playerWidth;
+        screenBoundaryRight = canvasRect.rect.width / 2 - playerWidth;
+
+        // Position the player at the bottom of the canvas
+        Vector3 startPosition = rectTransform.anchoredPosition;
+        startPosition.y = -canvasRect.rect.height / 2 + playerWidth;
+        rectTransform.anchoredPosition = startPosition;
     }
 
     void Update()
     {
         // Get horizontal input (left/right arrow keys)
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        
+
+        // Debugging user input
+        if (horizontalInput < 0)
+        {
+            Debug.Log("left");
+        }
+        else if (horizontalInput > 0)
+        {
+            Debug.Log("right");
+        }
+
         // Calculate new position
-        Vector3 newPosition = transform.position + new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0);
-        
+        Vector2 newPosition = rectTransform.anchoredPosition + new Vector2(horizontalInput * moveSpeed * Time.deltaTime, 0);
+
         // Clamp position to screen boundaries
         newPosition.x = Mathf.Clamp(newPosition.x, screenBoundaryLeft, screenBoundaryRight);
-        
+
         // Update position
-        transform.position = newPosition;
+        rectTransform.anchoredPosition = newPosition;
     }
 }
